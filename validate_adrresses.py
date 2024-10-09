18565a87006e5ef86612ce9733e5917eccef9f12
luis-ntonio <luis.gutierrez.g@utec.edu.pe>
2024-10-09 15:41:15 -0500

    a

--- validate_adrresses.py
+++ validate_adrresses.py
@@ -5,7 +5,7 @@ import re
 
 def remove_duplicates_preserve_order(address):
     # Step 1: Split the address into words and preserve the original order
-    address_parts = address.replace('.', ' ').replace(',', ' ').split("REFERENCIA")[0].split("CERCA ")[0].split()
+    address_parts = address.replace('.', ' ').replace(',', ' ').split("REFERENCIA")[0].split("CERCA ")[0].split("LLAMAR ")[0].split("FRENTE ")[0].split("AL LADO ")[0].split()
 
     # Step 2: Remove duplicates while preserving order
     seen = set()
@@ -20,7 +20,13 @@ def remove_duplicates_preserve_order(address):
     
     pattern=r'\b[A-Z]+\s+DEL\s+[A-Z]+\b'
     cleaned_address = re.sub(pattern, lambda m: m.group(0).replace(' ', '_'), cleaned_address)
-    pattern=r'\b[A-Z]+\s+LA\s+[A-Z]+\b'
+    pattern=r'\b[A-Z]+\s+DE LA\s+[A-Z]+\b'
+    cleaned_address = re.sub(pattern, lambda m: m.group(0).replace(' ', '_'), cleaned_address)
+    cleaned_address = re.sub(pattern, lambda m: m.group(0).replace(' ', '_'), cleaned_address)
+    pattern=r'\bLA\s+[A-Z]+\b'
+    cleaned_address = re.sub(pattern, lambda m: m.group(0).replace(' ', '_'), cleaned_address)
+    cleaned_address = re.sub(pattern, lambda m: m.group(0).replace(' ', '_'), cleaned_address)
+    pattern=r'\bLAS\s+[A-Z]+\b'
     cleaned_address = re.sub(pattern, lambda m: m.group(0).replace(' ', '_'), cleaned_address)
     pattern=r'\bLOS\s+[A-Z]+\b'
     cleaned_address = re.sub(pattern, lambda m: m.group(0).replace(' ', '_'), cleaned_address)
@@ -32,7 +38,13 @@ def remove_duplicates_preserve_order(address):
     pattern=r'PASAJE\s+[0-9]+\b'
     cleaned_address = re.sub(pattern, lambda m: m.group(0).replace(' ', '_'), cleaned_address)
     
+    pattern=r'PASAJE\s+[A-Z]+\b'
+    cleaned_address = re.sub(pattern, lambda m: m.group(0).replace(' ', '_'), cleaned_address)
+    
     pattern=r'\bSAN\s+[A-Z]+\b'
+    cleaned_address = re.sub(pattern, lambda m: m.group(0).replace(' ', '_'), cleaned_address)
+    
+    pattern=r'\bPJE\s+[A-Z]+\b'
     cleaned_address = re.sub(pattern, lambda m: m.group(0).replace(' ', '_'), cleaned_address)
     
     pattern=r'\bB\s+[0-9]+\b'
@@ -61,7 +73,7 @@ def validate_address(address: str, pais: str = 'Chile') -> str:
     address_parts = address_parts.split(" ")
     for i, ad in enumerate(address_parts):
         address_parts[i] = ad.replace("_", " ")
-    iter = 3 if len(address_parts) > 3 else 2 
+    iter = 2
 
     with ThreadPoolExecutor() as executor:
         futures = []
