32a2fa9b054c81749492c8d81c9aed159517a267
luisgutierrez_belcorp <luisgutierrez@belcorp.biz>
2024-11-18 13:04:08 -0500

    a

--- validate_adrresses.py
+++ validate_adrresses.py
@@ -13,21 +13,22 @@ def remove_duplicates_preserve_order(address):
 
     # Step 3: Create a single regex to handle all patterns at once
     patterns = [
-        (r'\b[A-Z]+\s+DEL\s+[A-Z]+\b', '_'),
-        (r'\b[A-Z]+\s+DE LA\s+[A-Z]+\b', '_'),
-        (r'\bLA\s+[A-Z]+\b', '_'),
-        (r'\bLAS\s+[A-Z]+\b', '_'),
-        (r'\bLOS\s+[A-Z]+\b', '_'),
-        (r'\bEL\s+[A-Z]+\b', '_'),
-        (r'PJE\s+[0-9]+\b', '_'),
-        (r'PASAJE\s+[0-9]+\b', '_'),
-        (r'PASAJE\s+[A-Z]+\b', '_'),
-        (r'\bSAN\s+[A-Z]+\b', '_'),
-        (r'\bPJE\s+[A-Z]+\b', '_'),
-        (r'\bB\s+[0-9]+\b', '_'),
-        (r'\bCASA\s+[0-9]+\b', '_'),
-        (r'\bN\s+[0-9]+\b', '_'),
-        (r'\bSECTOR\s+[0-9]+\b', '_')
+        (r'\b[A-Z]+\s+[0-9]+', '_'),
+        (r'\b[A-Z]+\s+DEL\s+[A-Z]+', '_'),
+        (r'\b[A-Z]+\s+DE LA\s+[A-Z]+', '_'),
+        (r'\bLA\s+[A-Z]+', '_'),
+        (r'\bLAS\s+[A-Z]+', '_'),
+        (r'\bLOS\s+[A-Z]+', '_'),
+        (r'\bEL\s+[A-Z]+', '_'),
+        (r'\b[A-Z]+\s+EL\s+[A-Z]+', '_'),
+        (r'\b[A-Z]+\s+CON\s+[A-Z]+', '_'),
+        (r'\b[A-Z]+\s+LOS\s+[A-Z]+', '_'),
+        (r'\b[A-Z]+\s+LA\s+[A-Z]+', '_'),
+        (r'\b[A-Z]+\s+LAS\s+[A-Z]+', '_'),
+        (r'\b[A-Z]+\s+CASA\s+[A-Z]+', '_'),
+        (r'PASAJE\s+[A-Z]+', '_'),
+        (r'\bSAN\s+[A-Z]+', '_'),
+        (r'\bPJE\s+[A-Z]+', '_')
     ]
 
     cleaned_address = ' '.join(unique_parts)
@@ -38,26 +39,30 @@ def remove_duplicates_preserve_order(address):
     cleaned_address = cleaned_address.replace('DE ', '').replace('AV ', '').replace('SN ', '').replace('SN', '').replace('  ', ' ')
     return cleaned_address
 
-def validate_address(address: str, pais: str = 'Chile') -> str:
-    address_parts = address.split(" ")
-    address_parts = [ad.replace("_", " ") for ad in address_parts]
-    iter = 2
 
+def validate_address(address: str, comuna:str, pais: str = 'Chile') -> str:
+    address_parts = address.split(" ")[:-1]
+    address_parts = [ad.replace("_", " ") for ad in address_parts]
+    #print(address_parts, "address_parts", comuna)
+    iter = 2 if len(address_parts) > 4 else 1
+   
     with ThreadPoolExecutor(max_workers=10) as executor:
         futures = []
 
-        for i in range(5, iter, -1):
+        for i in range(6, iter, -1):
             val_addrs = combinations(address_parts, i)
-            
             # Filter combinations with at least two consecutive words
-            valid_combinations = [
-                val_add for val_add in val_addrs
-                if has_consecutive_words(val_add, address_parts)
-            ]
+            valid_combinations = [address_parts]
 
-            # Submit the filtered combinations asynchronously to the ThreadPoolExecutor
+            for val_add in val_addrs:
+                indices = [address_parts.index(word) for word in val_add if word in address_parts]
+
+                if any(indices[i] + 1 == indices[i + 1] for i in range(len(indices) - 1)):
+                    valid_combinations.append(val_add)
+
             for val_add in valid_combinations:
-                future = executor.submit(get_coordinates, f'{" ".join(val_add)}, {pais}')
+                dir = f'{" ".join(val_add)} {comuna}, {pais}'
+                future = executor.submit(get_coordinates, dir)
                 futures.append(future)
 
         # Process futures as they complete
@@ -73,13 +78,7 @@ def validate_address(address: str, pais: str = 'Chile') -> str:
             except Exception as e:
                 continue  # Handle the exception, but don't stop processing other futures
 
-    print("sin coincidencias", address_parts, " ------ ", address)
+    print("sin coincidencias", address_parts, " ------ ", address, comuna)
     return None, None, None
 
 
-def has_consecutive_words(combination, original_parts):
-    """
-    Check if a combination contains at least two consecutive words from the original address parts.
-    """
-    indices = [original_parts.index(word) for word in combination if word in original_parts]
-    return any(indices[i] + 1 == indices[i + 1] for i in range(len(indices) - 1))
